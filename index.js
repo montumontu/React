43accda8413f42ab294ed34180343b613b221eb3
nayan <nayan.p@talview.com>
2023-01-14 21:10:14 +0530

    react project setup

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const heading1 = React.createElement("h1", { id: "title" }, "Heading 1");
+const heading2 = React.createElement("h1", { id: "title" }, "Heading 2");
+const container = React.createElement("div", {}, [ heading1, heading2 ]);
+
+const heading = React.createElement("h1", {}, "Namaste");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
